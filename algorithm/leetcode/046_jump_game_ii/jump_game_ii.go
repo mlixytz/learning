04538eeb5b114d926c6808a3ef5b18f0ad9426b2
zhiqiang <shizhiqiang@mifanli.com>
2019-04-16 13:01:49 +0800

    046 done!

---
+++ algorithm/leetcode/046_jump_game_ii/jump_game_ii.go
@@ -0,0 +1,25 @@
+package leetcode046
+
+/* 思路：贪心算法， 每次找这一跳之后可以去的最远距离 */
+
+func jump(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	count, cur := 0, 0
+	for cur < n-1 {
+		count++
+		max, _cur := 0, 0
+		for i := 1; i <= nums[cur]; i++ {
+			if cur+i == n-1 { // 如果cur+i等于n-1 说明这一跳可以到达列表尾部，所以直接返回。
+				return count
+			}
+			if (cur+i)+nums[cur+i] > max {
+				max, _cur = (cur+i)+nums[cur+i], cur+i
+			}
+		}
+		cur = _cur
+	}
+	return count
+}
---
+++ algorithm/leetcode/046_jump_game_ii/jump_game_ii_test.go
@@ -0,0 +1,15 @@
+package leetcode046
+
+import "testing"
+
+func TestSolution(t *testing.T) {
+	if jump([]int{2, 1}) != 1 {
+		t.Error("test error")
+	}
+	if jump([]int{3, 2, 1}) != 1 {
+		t.Error("test error")
+	}
+	if jump([]int{2, 3, 1, 1, 1}) != 2 {
+		t.Error("test error")
+	}
+}
