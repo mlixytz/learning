bc3bcbf7ec09337ce7278645e08e02620e6eb894
zhiqiang.shi <zhiqiang.shi@opay-inc.com>
2019-10-31 11:38:09 +0800

    add a simple spring project for understanding spring

---
+++ java/spring/src/com/mlixytz/spring/beans/Student.java
@@ -0,0 +1,47 @@
+package com.mlixytz.spring.beans;
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+    public Student(int id, String name, int age) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
