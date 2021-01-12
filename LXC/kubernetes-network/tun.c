f102ae9609f8d6cc934bd60fb468a746fa592e50
mlixytz <mlixytz@gmail.com>
2021-01-12 23:28:12 +0800

    增加tun程序

---
+++ LXC/kubernetes-network/tun.c
@@ -0,0 +1,53 @@
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <linux/if_tun.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int tun_alloc(int flags) {
+    struct ifreq ifr;
+    int fd, err;
+    char *clonedev = "/dev/net/tun"
+
+    if ((fd = open(clonedev, O_RDWR)) < 0) {
+        return fd;
+    }
+
+    memset(&ifr, 0, sizeof(ifr));
+    ifr.ifr_flags = flags;
+
+    if ((err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0) {
+        close(fd);
+        return err;
+    }
+
+    printf("Open tun/tap device: %s for reading...\n", ifr.ifr_name);
+
+    return fd;
+}
+
+int main() {
+    int tun_fd, nread;
+    char buffer[1500];
+
+    tun_fd = tun_alloc(IFF_TUN | IFF_NO_IP);
+
+    if (tun_fd < 0) {
+        perror("Allocating interface");
+        exit(1);
+    }
+    while(1) {
+        nread = read(tun_fd, buffer, sizeof(buffer));
+        if (nread < 0) {
+            perror("Reading from interface");
+            close(tun_fd);
+            exit(1);
+        }
+        printf("Read %d bytes from tun/tap device\n", nread);
+    }
+    return 0;
+}
